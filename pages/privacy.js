579870e1d1389844577f6900ff6feae74cf15a4f
Oleg Frolov <frolololeg@gmail.com>
2023-04-13 12:44:54 +0100

    + strong

--- pages/privacy.js
+++ pages/privacy.js
@@ -5,7 +5,7 @@ export default function Home() {
             <div className="privacyContainer">
                 <h1>Privacy Policy</h1>
                 <div className="VSpacer48"></div>
-                <p>Last updated: April 8, 2023</p>
+                <p><strong>Last updated: April 8, 2023</strong></p>
                 <div className="VSpacer48"></div>
                 <p>
                     BoxelXR, ("BoxelXR," "we," "our," or "us") is committed to protecting the privacy of our users. This Privacy Policy explains our data practices when you visit our website, use our BoxelXR application ("App"), or interact with our services on the Quest platform (collectively, the "Services"). By using the Services, you consent to the practices described in this Privacy Policy.
@@ -15,38 +15,38 @@ export default function Home() {
                     We reserve the right to make changes to this Privacy Policy at any time and for any reason. We will alert you about any changes by updating the "Last updated" date of this Privacy Policy. You are encouraged to periodically review this Privacy Policy to stay informed of updates.
                 </p>
                 <div className="VSpacer24"></div>
-                <p>1. No Information Collection</p>
+                <p><strong>1. No Information Collection</strong></p>
                 <div className="VSpacer24"></div>
                 <p>
                     BoxelXR is designed with your privacy in mind. We do not collect, store, or process any personal information or user-generated content when you use our Services. All data, including voxel graphics and user preferences, are stored locally on your device and are not transmitted to or accessed by BoxelXR.
                 </p>
                 <div className="VSpacer24"></div>
-                <p>2. Data Security</p>
+                <p><strong>2. Data Security</strong></p>
                 <div className="VSpacer24"></div>
                 <p>
                     As we do not collect or store any user data, there is no risk of unauthorized access, alteration, or destruction of your information while using our Services. However, we recommend that you take appropriate measures to secure your device and the information stored on it.
                 </p>
                 <div className="VSpacer24"></div>
-                <p>3. No Third-Party Websites and Services</p>
+                <p><strong>3. No Third-Party Websites and Services</strong></p>
                 <div className="VSpacer24"></div>
                 <p>
                     Our Services do not contain any links to third-party websites or services. We are dedicated to providing a seamless and secure user experience without exposing you to potential privacy risks associated with third parties.
                 </p>
                 <div className="VSpacer24"></div>
-                <p>4. Children's Privacy</p>
+                <p><strong>4. Children's Privacy</strong></p>
                 <div className="VSpacer24"></div>
                 <p>
                     Our Services are not intended for use by children under the age of 13. As we do not collect personal information, we have no way of verifying the age of our users. However, if you believe that a child under the age of 13 is using our Services, please contact us so that we can investigate and take appropriate action.
                 </p>
                 <div className="VSpacer24"></div>
-                <p>5. Your Rights and Choices</p>
+                <p><strong>5. Your Rights and Choices</strong></p>
                 <div className="VSpacer24"></div>
                 <p>
                     Since we do not collect or store any personal information, there is no need for you to review, update, or delete any data through our Services. However, you can manage your voxel graphics and user preferences directly on your device.
                 </p>
                 <div className="VSpacer24"></div>
                 <p>
-                    By using our Services, you acknowledge and agree to the practices outlined in this Privacy Policy. If you have any questions or concerns about our privacy practices, please contact us at <strong>olegfrolovdesignr@gmail.com</strong>.
+                    By using our Services, you acknowledge and agree to the practices outlined in this Privacy Policy. If you have any questions or concerns about our privacy practices, please contact us at <strong>olegfrolovdesign@gmail.com</strong>.
                 </p>
                 <div className="VSpacer48"></div>
                 <div className="VSpacer48"></div>
